f0e6278de84bc6cc2ab65b91f0ec086fcb87b171
Hamza Ali <hamzu792@gmail.com>
2023-02-10 16:26:28 +0500

    ch_9 completed

---
+++ ch_9/app.js
@@ -0,0 +1,5 @@
+const fistName=prompt('Eneter your first name:')
+const lastName=prompt('Enter your last name:')
+const age=prompt('Enter your age:')
+
+alert(`Hello ${fistName} ${lastName}, you are ${age} years old.`)
